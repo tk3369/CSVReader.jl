e6216b72c0e11ca24787f26fd81093682770b096
Tom Kwong <tk3369@gmail.com>
2019-03-15 21:59:30 -0700

    major updates with test suite & experimental code

--- src/CSVReader.jl
+++ src/CSVReader.jl
@@ -6,11 +6,14 @@ using Parsers: tryparse
 
 export @parsers_str
 
+# This is twice as fast as the regular strip() function
+faststrip(x, quotechar) = length(x) > 2 ? (x[1] == x[end] == quotechar ? x[2:end-1] : x) : x
+
 # Parsers
 parse_string(s) = intern(s)
 parser_return_type(::Val{parse_string}) = Union{String, Missing}
 
-parse_float64(s::AbstractString) = something(tryparse(s, Float64), missing)
+parse_float64(s) = something(tryparse(s, Float64), missing)
 parser_return_type(::Val{parse_float64}) = Union{Float64, Missing}
 
 parse_int(s) = something(tryparse(s, Int), missing)
@@ -44,18 +47,14 @@ end
 """
 Read CSV file
 """
-function read_csv(filename; headers = true, delimiter = ',', quotechar = '"')
-    parsers = infer_parsers(filename, headers, delimiter, quotechar)
-    read_csv(filename, parsers, headers = headers, delimiter = delimiter)
-end
-
-"""
-Read CSV file with user specified parsers
-"""
-function read_csv(filename, parsers; headers = true, delimiter = ',', quotechar = '"')  
+function read_csv(filename, parsers = nothing;
+                    headers = true, 
+                    delimiter = ',', 
+                    quotechar = '"')  
+    parsers = something(parsers, infer_parsers(filename, headers, delimiter, quotechar))
     est_lines = estimate_lines_in_file(filename, with_headers = headers)
     open(filename) do f
-        hdr = read_headers(f, headers, delimiter)
+        hdr = read_headers(f, headers, delimiter, quotechar)
         r = 0
         preallocate_rows = est_lines
         df = make_dataframe(hdr, parser_return_type.(Val.(parsers)), preallocate_rows)
@@ -77,9 +76,9 @@ end
 """
 Peek into first few lines of CSV file
 """
-function peek_csv(filename; headers = true, nrows = 5, delimiter = ',')
+function peek_csv(filename; headers = true, nrows = 5, delimiter = ',', quotechar = '"')
     open(filename) do f
-        headers = read_headers(f, headers, delimiter)
+        headers = read_headers(f, headers, delimiter, quotechar)
         rows = read_first_few_lines(f, nrows)
         column_names = [mysplit(row, delimiter = delimiter) for row ∈ rows]
         ncols = maximum(length(x) for x ∈ column_names)
@@ -99,8 +98,8 @@ end
 
 function parse_line!(df, row, parsers, line, delimiter, quotechar)
     strings = mysplit(line, delimiter = delimiter, quotechar = quotechar)
-    for (i, p) ∈ enumerate(parsers)
-        parsed_value = p(strings[i])
+    for (i, parser) ∈ enumerate(parsers)
+        parsed_value = faststrip(strings[i], quotechar) |> parser
         #@debug "Parsed line" row i parsed_value
         df[row,i] = parsed_value
     end
@@ -115,7 +114,6 @@ function make_dataframe(hdr, types, rows)
     hdr = something(hdr, ["_$i" for i ∈ 1:length(types)])
     cols = [Vector{T}(undef, rows) for T ∈ types]
     df = DataFrame(cols, Symbol.(hdr), makeunique = true)
-    #@show df
     df
 end
 
@@ -124,9 +122,9 @@ function trim_table(df, rows)
     df[1:rows, :]
 end
 
-function read_headers(f, headers, delimiter)
+function read_headers(f, headers, delimiter, quotechar)
     if headers
-        mysplit(readline(f), delimiter = delimiter)
+        map(h -> faststrip(h, quotechar), mysplit(readline(f), delimiter = delimiter))
     else
         nothing
     end
@@ -210,9 +208,121 @@ function read_first_few_lines(f, lines_to_read)
 end
 
 function mysplit(str::AbstractString; delimiter::AbstractChar = ',', quotechar::AbstractChar = '"', strip_quotes = true) 
-    stripfunc(x) = x[1] == quotechar && x[end] == quotechar ? x[2:end-1] : x
-    f = strip_quotes ? stripfunc : identity
-    map(f, split(str, delimiter))
+    split(str, delimiter)
+end
+
+# -------------------
+# Experimental stuffs
+# -------------------
+
+# DataFrame approach but have a parsing state machine
+function read_csv2(filename, parsers = nothing; 
+                    headers = true, 
+                    delimiter = ',', 
+                    quotechar = '"')  
+    parsers = something(parsers, infer_parsers(filename, headers, delimiter, quotechar))
+    est_lines = estimate_lines_in_file(filename, with_headers = headers)
+    open(filename) do f
+        hdr = read_headers(f, headers, delimiter, quotechar)
+        r = 0
+        preallocate_rows = est_lines
+        df = make_dataframe(hdr, parser_return_type.(Val.(parsers)), preallocate_rows)
+        @inbounds while !eof(f)
+            line = readline(f)
+            r += 1
+            if r > preallocate_rows
+                new_size = max(floor(Int, preallocate_rows * 1.05 + 5), preallocate_rows + 5)
+                @debug "Growing data frame from $preallocate_rows rows to $new_size"
+                grow_dataframe!(df, new_size)
+                preallocate_rows = new_size
+            end 
+            # This state machine keeps track of two indices (i,j).
+            # The value of `j` is incremented until it hits a delimiter or end of string.
+            # The value of `k` is then determined as `j-1` if line[j] is a delimiter,
+            # else `k` would have the same index as `end` of line.
+            # The cell is then consumed i.e. stripped, parsed, and set.
+            # Reference: i = start, j = pointer, k = end, c = column index
+            len = length(line)
+            i = j = c = 1
+            while i <= len && j <= len
+                if line[j] == delimiter || j == len
+                    k = (j == len) ? len : j-1  # special case at end of line
+                    df[r, c] = faststrip(line[i:k], quotechar) |> parsers[c]
+                    c += 1
+                    i = j = j+1
+                else
+                    j += 1
+                end
+            end
+            # parse_line!(df, r, parsers, line, delimiter, quotechar)
+        end
+        trim_table(df, r)
+    end
+end
+
+# Return vector of named tuples
+function read_csv_nt(filename, parsers = nothing; 
+                        headers = true, 
+                        delimiter = ',', 
+                        quotechar = '"')  
+    parsers = something(parsers, infer_parsers(filename, headers, delimiter, quotechar))
+    result = Vector{NamedTuple}()
+    open(filename) do f
+        # header symbols
+        hdr = read_headers(f, headers, delimiter, quotechar)
+        hdr = something(hdr, ["_$i" for i ∈ 1:length(parsers)])
+        hdr = Tuple(Symbol.(hdr))
+        ncol = length(parsers)
+        r = 0
+        v = Any[nothing for _ in 1:ncol]
+        T = NamedTuple{hdr}
+        while !eof(f)
+            line = readline(f)
+            r += 1
+            s = mysplit(line, delimiter = delimiter, quotechar = quotechar)
+            @inbounds for i in 1:ncol
+                v[i] = faststrip(s[i], quotechar) |> parsers[i]
+            end
+            push!(result, T(v))
+        end
+    end
+    result
+end
+
+# Return vector of named tuples but also have a parsing state machine
+function read_csv_nt2(filename, parsers = nothing; 
+                    headers = true, 
+                    delimiter = ',', 
+                    quotechar = '"')  
+    parsers = something(parsers, infer_parsers(filename, headers, delimiter, quotechar))
+    open(filename) do f
+        # header symbols
+        hdr = read_headers(f, headers, delimiter, quotechar)
+        hdr = something(hdr, ["_$i" for i ∈ 1:length(parsers)])
+        hdr = Tuple(Symbol.(hdr))
+        ncol = length(parsers)
+        r = 0
+        v = Any[nothing for _ in 1:ncol]
+        T = NamedTuple{hdr}
+        result = Vector{T}()
+        while !eof(f)
+            line = readline(f)
+            len = length(line)
+            i = j = c = 1
+            while i <= len && j <= len
+                if line[j] == delimiter || j == len
+                    k = (j == len) ? len : j-1  # special case at end of line
+                    v[c] = faststrip(line[i:k], quotechar) |> parsers[c]
+                    c += 1
+                    i = j = j+1
+                else
+                    j += 1
+                end
+            end
+            push!(result, T(v))
+        end
+        result
+    end
 end
 
 end # module
