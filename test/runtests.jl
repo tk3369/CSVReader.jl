8914038275afe070605307152f40046d871bc230
Tom Kwong <tk3369@gmail.com>
2020-07-03 23:20:32 -0700

    updated tests, bumped version

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ const iris = "iris.csv"
 
 function testiris(df)
     @test size(df) == (150, 6)
-    @test names(df) == Symbol.(["id", "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species"])
+    @test names(df) == ["id", "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species"]
     @test unique(df[!, :Species]) == ["setosa", "versicolor", "virginica"]
     @test [sum(df[!, i]) for i in 2:5] ≈ [876.5, 458.6, 563.7, 179.9]
 end
